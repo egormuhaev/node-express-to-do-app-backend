5fbb98d7fc3ca373c556621ceda0cd972b6092cd
login <egormuhaev@gmail.com>
2022-12-28 02:34:08 +0300

    swagger

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -1,7 +1,81 @@
 const db = require('../config/configPostgres')
 
-class TaskController {
 
+class TaskController {
+    async addNewTask(req, res){
+        const {
+            name,
+            discription, 
+            deadline, 
+            notification, 
+            tags, 
+            important, 
+            group, 
+            user_id} = req.body
+        try {
+            if (group === ""){
+                const defaultGroup = await db.query("SELECT * FROM grouptask WHERE user_id = $1 AND name = 'default'", [user_id])
+                if (defaultGroup.rows[0].id){
+                    group = defaultGroup.rows[0].id
+                }
+                else {
+                    res.json({message: "Error"})
+                }
+            }
+        
+            const newTask = await db.query('INSERT INTO task (name, description, deadline, notification, tags, important, status, group_id, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *', 
+            [name, discription, deadline, notification, tags, important, false, group, user_id])
+            res.json(newTask.rows)
+        }
+        catch(e){
+            console.log(e)
+            res.json({message: e})
+        }
+        
+    }
+
+    async deleteTaskById(req, res){
+        const {id} = req.body
+        try {
+            const delTask = await db.query('DELETE INTO task WHERE id = $1 RETURNING *', [id]) 
+            res.json(delTask.rows)
+        }
+        catch(e){
+            console.log(e)
+            res.json({message: e})
+        }
+    }
+
+    async comleteTaskById(req, res){
+        const {id} = req.body
+        try {
+            const tasks = await db.query('SELECT * FROM task WHERE id = $1', [id])
+            const newTaskStatus =  !Boolean(tasks.rows[0].status)
+            const updateTask = await db.query('UPDATE task SET stasus = $1 WHERE id = $2 RETURNING *', [id, newTaskStatus])
+            res.json(updateTask.rows)
+        }
+        catch(e){
+            res.json(e)
+        }
+    }
+
+    async taskAddToGroup(req, res){
+        const {task_id, group_id} = req.body
+        try{   
+            const updateTask = await db.query('UPDATE task SET group_id = $1 WHERE id = $2 RETURNING *', [group_id, task_id])
+        }
+        catch(e){
+            res.json(e)
+        }
+    }
+
+    async setImpTaskById(){
+        
+    }
+
+    async getAllTaskByUserId(){
+
+    }
 }
 
 module.exports = new TaskController()
--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express =  require('express');
 const cors = require('cors');
 const userRouter = require('./routes/users.routes')
 const groupRouter = require('./routes/group.routes')
-
+const taskRouter = require('./routes/task.routes')
 
 const app = express();
 const PORT = process.env.PORT || 3001
 
-app.use(cors());
-app.use(express.json()); 
+try {
+    app.use(cors());
+    app.use(express.json()); 
+}
+catch(e) {
+    console.log(e)
+}
+
+
 app.use('/api', userRouter)
 app.use('/api', groupRouter)
+app.use('/api', taskRouter)
+app.use('/', taskRouter)
 
 
 app.listen(PORT, (error) => {
